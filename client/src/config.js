926d785d702f88731a1ea29013392bc72f638b12
Hardiksinh Rajput <98508248+lucifer7875@users.noreply.github.com>
2023-12-22 12:49:59 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 module.exports = global.config = {
     SECRET_KEY: {
-        STRIPE_SECRET_KEY: "pk_test_51NYqdVSDmNa4pMqXQpmmYGMeC0QIKW6udnSlel5oqmAg1tygz4CzxSaV9ryFfKTDZK9RmeiW7FEW1crTJbOBziuj00zbIFTBko"
+        STRIPE_SECRET_KEY: "Enter your secret key of stripe"
     }
 };
